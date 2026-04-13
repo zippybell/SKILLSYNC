563d934ae04eec96e6b189a16ef985851319d37a
Blqissafirra9 <soraa4518@gmail.com>
2026-04-13 17:25:04 +0700

    Add files via upload

---
+++ Career_Gap.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - SkillSync</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-blue-50 text-gray-800">
+
+    <!-- NAVBAR -->
+    <nav class="bg-white shadow px-6 py-4 flex justify-between items-center">
+        <h1 class="text-xl font-bold text-blue-600">SkillSync</h1>
+        <div class="flex items-center space-x-4">
+            <span>Halo, Mahasiswa</span>
+            <a href="logout.php" 
+               class="bg-blue-500 text-white px-4 py-1.5 rounded hover:bg-blue-600 transition">
+                Logout
+            </a>
+        </div>
+    </nav>
+
+    <!-- HERO SECTION -->
+    <section class="text-center pt-36 pb-20">
+        <h1 class="text-4xl font-semibold text-blue-700 mb-4">
+            Selamat Datang di SkillSync
+        </h1>
+        <p class="text-blue-600 text-lg">
+            CAREER GAP SCANNER
+        </p>
+    </section>
+
+</body>
+</html>
